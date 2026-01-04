dc3c51fcd4922a8616ab64c5f2cd02c477f98328
Silver <76074145+Kusomadoshi@users.noreply.github.com>
2026-01-04 02:00:38 +0100

    Program to train sentence piece
    Program to train sentence piece using the 4Go Oscar corpus
    Update : adapted the code for big corpus

--- camemBERT_Small_2/create_tokenizer/train_spm.py
+++ camemBERT_Small_2/create_tokenizer/train_spm.py
@@ -2,7 +2,7 @@ import sentencepiece as spm
 from pathlib import Path
 
 ##ENTRAINEMENT
-CORPUS = "data/raw_shards/train_shard_*.txt"  # ou un fichier concaténé corpus_all.txt
+CORPUS = "/home/silver/Oscar/corpus_utf8.txt"  # ou un fichier concaténé corpus_all.txt
 OUT_DIR = Path("models")
 OUT_DIR.mkdir(parents=True, exist_ok=True)
 
@@ -10,7 +10,6 @@ model_prefix = str(OUT_DIR / "spm")
 vocab_size = 32000
 model_type = "unigram"   # ou "bpe"
 character_coverage = 1.0
-input_sentence_size = 2000000  # échantillon si corpus très grand
 
 spm.SentencePieceTrainer.Train(
     input=CORPUS,
@@ -18,8 +17,7 @@ spm.SentencePieceTrainer.Train(
     vocab_size=vocab_size,
     model_type=model_type,
     character_coverage=character_coverage,
-    input_sentence_size=input_sentence_size,
-    shuffle_input_sentence=True,
+    train_extremely_large_corpus = True,
     hard_vocab_limit=False
 )
 
