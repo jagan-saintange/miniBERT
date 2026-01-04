155119cc05fc3cfd20ce57064b22acb07af9c6bd
Silver <76074145+Kusomadoshi@users.noreply.github.com>
2026-01-04 13:48:48 +0100

    Update clean_corpus.py
    add comments

--- camemBERT_Small_2/Oscar_corpus/clean_corpus.py
+++ camemBERT_Small_2/Oscar_corpus/clean_corpus.py
@@ -2,12 +2,14 @@ from pathlib import Path
 import unicodedata
 import os
 import chardet
+
 os.chdir(r"/home/silver/Oscar")
 
 
 input_file = "corpus_sp.txt"
 output_file = "corpus_utf8.txt"
 
+#boucle pour essayer de convertir chaque ligne en encodage utf-8
 with open(input_file, "rb") as f_in, open(output_file, "w", encoding="utf-8") as f_out:
     for raw_line in f_in:
         # Détection
@@ -22,7 +24,7 @@ with open(input_file, "rb") as f_in, open(output_file, "w", encoding="utf-8") as
                 # fallback si l'encodage détecté échoue
                 line = raw_line.decode("latin-1", errors="replace")
         else:
-            # si chardet ne sait pas → fallback direct
+            # fallback direct car on ne connait pas
             line = raw_line.decode("latin-1", errors="replace")
 
         f_out.write(line)
